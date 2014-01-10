3090feeb8c40b60c63ab89a55de3b6d66b6b8cc1
Wylie Fisher <WFisher@ebsco.com>
2014-01-10 09:36:52 -0500

    It works

--- js/master.js
+++ js/master.js
@@ -1,29 +1,28 @@
 $(function() {
 
-  $(document).on('click','.submit',function() {
-    var val = $('.input').val();
-    var val = val.replace(/[^a-zA-Z 0-9]+/g,'');
-    var val = val.split(' ');
-    
-    var arr = ['ass','jerk'];
+	$(document).on('click','.submit',function() {
+		var val = $('.input').val();
+		var val = val.replace(/[^a-zA-Z 0-9]+/g,'');
+		var val = val.split(' ');
+		
+		var badWords = ['ass','jerk'];
 
-    for(var x = 0; x < arr.length; x++) {
-      console.log(arr[x]);
-    }
-    
-    for(var i = 0; i < val.length; i++) {
-      if(val[i] === 'ass') {
-        var bad = true;
-      }
+		for(var x = 0; x < badWords.length; x++) {
+			for(var i = 0; i < val.length; i++) {
+					if(val[i] === badWords[x]) {
+						var bad = true;
+						console.log(badWords[x]);
+						if (bad) {
+							$('body').css('background-color', 'red');
+							$('.result').text('You are naughty!').removeClass('naughty').addClass('naughty');
+						}
+					} else {
+						$('.result').text('Yes, Santa Commands It!').removeClass('naughty').addClass('nice');
+					}
+				}
+			}
+		});
 
-      if (bad) {
-        $('body').css('background-color', 'red');
-      } else {
-        $('body').css('background-color', 'green');
-      }
-    }
-  });
-
-  // compare the two results
+	// compare the two results  
 
 });
