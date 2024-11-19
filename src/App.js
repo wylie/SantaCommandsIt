43b005aba1010c894bf995c3e737cfafe358ebe0
Wylie Fisher <me@wyliefisher.com>
2024-11-19 09:36:32 -0500

    adding a feature flag as well as some new markup and styling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import featureFlags from './config';
 
 function App() {
   return (
-    <div className="app">
+    <>
       {featureFlags.redesign ? (
-        <>
-          <h1>Ho Ho Ho!</h1>
-          <img src={Santa} alt="Santa" />
-        </>
-    ) : (
-        <>
+        <div className="app flag">
+          <div className="img">
+            <img src={Santa} alt="Santa" />
+          </div>
+          <div className="chats">
+            <h1 className="chat santa">Ho Ho Ho!</h1>
+            <h1 className="chat santa">Whataya want?</h1>
+            <p className="chat user">I'd like a pony!</p>
+          </div>
+        </div>
+      ) : (
+        <div className="app main">
           <h1>Santa Commands It!</h1>
           <img src={Santa} alt="Santa" />
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
