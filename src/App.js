9f9dc7ffd712c5158d4dd4b30b6c01675f8cdd9f
Wylie Fisher <me@wyliefisher.com>
2024-11-20 09:48:01 -0500

    - clean up the CSS to have inputs in the chats - change the H1 to a P - add a hidden H1 at the top of the page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Santa from "./assets/santa.png";
 import featureFlags from './config';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log("I'd like", inputValue);
+    }
+  };
+
   return (
     <>
       {featureFlags.redesign ? (
-        <div className="app flag">
-          <div className="img">
-            <img src={Santa} alt="Santa" />
+        <>
+          <h1>Santa Commands It!</h1>
+          <div className="app flag">
+            <div className="img">
+              <img src={Santa} alt="Santa" />
+            </div>
+            <div className="chats">
+              <p className="chat santa">Ho Ho Ho!</p>
+              <p className="chat santa">Whataya want?</p>
+              <p className="chat user">
+                I'd like 
+                <input 
+                  type="text" 
+                  placeholder="type what you want" 
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  onKeyPress={handleKeyPress}
+                />
+              </p>
+            </div>
           </div>
-          <div className="chats">
-            <h1 className="chat santa">Ho Ho Ho!</h1>
-            <h1 className="chat santa">Whataya want?</h1>
-            <p className="chat user">I'd like a pony!</p>
-          </div>
-        </div>
+        </>
       ) : (
         <div className="app main">
           <h1>Santa Commands It!</h1>
