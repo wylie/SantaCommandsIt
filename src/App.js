46d83be59d87ca106746f6bec13e0433be5894ad
Wylie Fisher <me@wyliefisher.com>
2024-12-02 10:49:56 -0500

    some minor changes - bringing back the H1 - remove commented out code - change the font-size for Santa's chats

--- src/App.js
+++ src/App.js
@@ -19,28 +19,25 @@ function App() {
   return (
     <>
       {featureFlags.redesign ? (
-        <>
-          <h1>Santa Commands It!</h1>
-          <div className="app flag">
-            <div className="img">
-              <img src={Santa} alt="Santa" />
-            </div>
-            <div className="chats">
-              <p className="chat santa">Ho Ho Ho!</p>
-              <p className="chat santa">Whataya want?</p>
-              <p className="chat user">
-                I'd like 
-                <input 
-                  type="text" 
-                  placeholder="type what you want" 
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  onKeyPress={handleKeyPress}
-                />
-              </p>
-            </div>
+        <div className="app flag">
+          <div className="img">
+            <img src={Santa} alt="Santa" />
           </div>
-        </>
+          <div className="chats">
+            <p className="chat santa">Ho Ho Ho!</p>
+            <p className="chat santa">Whataya want?</p>
+            <p className="chat user">
+              I'd like 
+              <input 
+                type="text" 
+                placeholder="type what you want" 
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+              />
+            </p>
+          </div>
+        </div>
       ) : (
         <div className="app main">
           <h1>Santa Commands It!</h1>
